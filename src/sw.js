66e971bcf4af080cc06557f4ab5d2466057529b5
marmooo <marmooo@users.noreply.github.com>
2021-05-24 09:43:10 +0900

    bootstrap5

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-05-04 20:10';
+var CACHE_NAME = '2021-05-24 09:40';
 var urlsToCache = [
   '/tegaki-100masu/',
   '/tegaki-100masu/index.js',
@@ -9,7 +9,7 @@ var urlsToCache = [
   '/tegaki-100masu/mp3/end.mp3',
   '/tegaki-100masu/mp3/correct3.mp3',
   'https://marmooo.github.io/fonts/textar-light.woff2',
-  'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js',
   'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js',
 ];
