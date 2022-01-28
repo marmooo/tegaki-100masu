5fa8e8e195d1aabf86f97de20a423dca980532a9
marmooo <marmooo@users.noreply.github.com>
2022-01-29 08:14:59 +0900

    bump signature_pad from 4.0.1 to 4.0.2

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-01-13 09:30';
+var CACHE_NAME = '2022-01-29 08:10';
 var urlsToCache = [
   "/tegaki-100masu/",
   "/tegaki-100masu/index.js",
@@ -9,7 +9,7 @@ var urlsToCache = [
   "/tegaki-100masu/mp3/correct3.mp3",
   "/tegaki-100masu/favicon/original.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.1/dist/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.2/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.9.0/dist/tf.min.js",
 ];
