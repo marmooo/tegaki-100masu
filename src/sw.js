041e8bc15cfabcd4413fa8aeef65a84615988722
marmooo <marmooo@users.noreply.github.com>
2022-09-11 08:55:10 +0900

    bump bootstrap from 5.2.0 to 5.2.1

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-09-01 00:15";
+var CACHE_NAME = "2022-09-11 08:49";
 var urlsToCache = [
   "/tegaki-100masu/",
   "/tegaki-100masu/index.js",
@@ -10,7 +10,7 @@ var urlsToCache = [
   "/tegaki-100masu/mp3/correct3.mp3",
   "/tegaki-100masu/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.0.7/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.20.0/dist/tf.min.js",
 ];
