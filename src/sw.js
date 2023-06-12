e3f6c4e3bd286fc7d9cefdbfc7a8ec4a4bc93b79
marmooo <marmooo@users.noreply.github.com>
2023-06-13 00:03:45 +0900

    update

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2023-05-21 14:10";
+var CACHE_NAME = "2023-06-13 00:00";
 var urlsToCache = [
   "/tegaki-100masu/",
   "/tegaki-100masu/index.js",
@@ -10,7 +10,6 @@ var urlsToCache = [
   "/tegaki-100masu/mp3/correct3.mp3",
   "/tegaki-100masu/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.5/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.6.0/dist/tf.min.js",
 ];
