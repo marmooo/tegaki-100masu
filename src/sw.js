dd58968f1a5698cddfe5616eb847973a03b64169
marmooo <marmooo@users.noreply.github.com>
2024-04-24 10:49:50 +0900

    improve user-select:none;

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2024-04-14 00:50";
+const CACHE_NAME = "2024-04-24 10:50";
 const urlsToCache = [
   "/tegaki-100masu/",
   "/tegaki-100masu/index.js",
@@ -10,7 +10,7 @@ const urlsToCache = [
   "/tegaki-100masu/mp3/correct3.mp3",
   "/tegaki-100masu/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.16.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.18.0/dist/tf.min.js",
 ];
 
 self.addEventListener("install", (event) => {
