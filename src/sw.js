42211d38e25eb6750b8d054ba8f35b0950975faf
marmooo <marmooo@users.noreply.github.com>
2024-05-11 16:19:21 +0900

    bump tfjs from 4.18.0 to 4.19.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2024-04-30 09:40";
+const CACHE_NAME = "2024-05-11 16:18";
 const urlsToCache = [
   "/tegaki-100masu/",
   "/tegaki-100masu/index.js",
@@ -10,7 +10,7 @@ const urlsToCache = [
   "/tegaki-100masu/mp3/correct3.mp3",
   "/tegaki-100masu/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.18.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.19.0/dist/tf.min.js",
 ];
 
 self.addEventListener("install", (event) => {
